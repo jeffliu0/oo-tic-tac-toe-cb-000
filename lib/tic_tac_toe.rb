6cda17d87ad66125fce01bd8e21f287e3e559c6a
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-04-05 06:37:03 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -4,6 +4,7 @@ class TicTacToe
   def initialize
     @board = [" ", " ", " ", " ", " ", " ",
        " ", " ", " "]
+       display_board
   end
 
   WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,4,8],
