02118938517e17ba0fc730f897be3148327f7d1f
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-04-05 06:25:58 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -3,9 +3,10 @@ class TicTacToe
 WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,4,8],
 [2,4,6], [0,3,6], [1,4,7], [2,5,8]]
 
-def initialize(board)
+def initialize
   @board = [" ", " ", " ", " ", " ", " ",
      " ", " ", " "]
+   end
 
 # Define display_board that accepts a board and prints
 # out the current state.
