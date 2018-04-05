93f09d94363ab8cecab72183a8549c65f6c6f6da
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-04-05 06:38:13 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -145,7 +145,7 @@ class TicTacToe
     end
 
     if won?
-      puts "Congratulations #{winner(board)}!"
+      puts "Congratulations #{winner)!"
     elsif draw?
       puts "Cat's Game!"
     end
